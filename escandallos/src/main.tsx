e2f49fd42173b69b6efb268cb8d9bf628c0976a8
Campillo <a.seguracampillo@gmail.com>
2026-08-03 13:16:56 +0200

    v0.4.5

--- escandallos/src/main.tsx
+++ escandallos/src/main.tsx
@@ -1,27 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { check } from "@tauri-apps/plugin-updater";
 import App from "./App";
 import "./index.css";
-
-async function checkForUpdates() {
-  try {
-    const update = await check();
-    if (update) {
-      const shouldUpdate = window.confirm(
-        `Nueva versión disponible: v${update.version}\n\n¿Deseas descargar e instalar ahora?\nLa app se reiniciará automáticamente.`
-      );
-      if (shouldUpdate) {
-        await update.downloadAndInstall();
-        const { relaunch } = await import("@tauri-apps/plugin-process");
-        await relaunch();
-      }
-    }
-  } catch (error) {
-    console.log("Error checking for updates:", error);
-  }
-}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
@@ -30,5 +11,3 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     </BrowserRouter>
   </React.StrictMode>,
 );
-
-setTimeout(checkForUpdates, 3000);
